b0b61744b9feaacdfac1b5e4a09cc3b3331ce501
oCrazyoff <walyssonribeiro3@gmail.com>
2025-04-16 13:47:48 -0300

    Add cache-busting query string to CSS link in cadastro.php and login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if (empty($_SESSION['_csrf'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eco Flow | Cadastre-se</title>
-    <link rel="stylesheet" href="assets/css/form_index.css">
+    <link rel="stylesheet" href="assets/css/form_index.css?v=<?php echo time(); ?>">
     <?php include("backend/includes/head.php") ?>
 </head>
 
