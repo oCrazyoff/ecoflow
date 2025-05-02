008e2dc52f9d8da218311f82abaaef2ed9764296
oCrazyoff <walyssonribeiro3@gmail.com>
2025-05-02 15:31:15 -0300

    Remove 'Relatório' button from dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -225,7 +225,6 @@ $totalNaoPago = array_sum(array_column($despesasNaoPagasDetalhadas, 'valor'));
     <div class="main-content">
         <div class="header">
             <h2>Saldo: R$ <?php echo number_format($rendaTotal - $totalNaoPago, 2, ',', '.') ?></h2>
-            <button onclick="mostrarAlert()">Relatório</button>
             <div class="data-container">
                 <button id="monthButton"><i class="bi bi-caret-down-fill"></i> </button>
                 <ul id="monthList" style="display: none;">
