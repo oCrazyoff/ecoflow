c35c9876df28cfea0cd422cc336a69b26acb4f13
oCrazyoff <walyssonribeiro3@gmail.com>
2025-05-07 00:35:46 -0300

    Implement code changes to enhance functionality and improve performance

--- index.php
+++ index.php
@@ -38,146 +38,151 @@ if (!isset($_SESSION['id']) && isset($_COOKIE['user_id'])) {
 </head>
 
 <body>
-    <div class="main-content">
-        <header id="header">
-            <div class="interface">
-                <div class="left">
-                    <img src="<?php echo BASE_URL ?>assets/img/logo.png" alt="Logo Eco Flow">
-                    <p>EcoFlow</p>
-                </div>
-                <div class="right">
-                    <nav>
-                        <a href="#">Início</a>
-                        <a href="#beneficios">Beneficios</a>
-                        <a href="#vantagens">Vantagens</a>
-                        <a href="#contato">Contato</a>
-                    </nav>
-                    <a id="btn-login" href="<?php echo BASE_URL ?>login.php">Login</a>
-                    <a id="btn-cadastro" href="<?php echo BASE_URL ?>cadastro.php">Cadastro</a>
-                </div>
+    <header id="header">
+        <div class="interface">
+            <div class="left">
+                <img src="<?php echo BASE_URL ?>assets/img/logo.png" alt="Logo Eco Flow">
+                <p>EcoFlow</p>
             </div>
-        </header>
-        <section id="hero">
-            <div class="interface">
-                <div class="left">
-                    <h1>Organize suas finanças com inteligência</h1>
+            <div class="right">
+                <nav>
+                    <a href="#">Início</a>
+                    <a href="#beneficios">Beneficios</a>
+                    <a href="#vantagens">Vantagens</a>
+                </nav>
+                <a id="btn-login" href="<?php echo BASE_URL ?>login.php">Login</a>
+                <a id="btn-cadastro" href="<?php echo BASE_URL ?>cadastro.php">Cadastro</a>
+            </div>
+        </div>
+    </header>
+    <section id="hero">
+        <div class="interface">
+            <div class="left">
+                <h1>Organize suas finanças com inteligência</h1>
+                <p>
+                    Simplifique o controle de rendas, despesas e investimentos em um só lugar. Tome decisões
+                    financeiras com confiança e alcance seus objetivos.
+                </p>
+                <a id="btn-cadastro" href="<?php echo BASE_URL ?>cadastro.php">Abrir conta</a>
+            </div>
+            <div class="right">
+                <img src="<?php echo BASE_URL ?>assets/img/index_img.png" alt="Hero">
+            </div>
+        </div>
+    </section>
+    <section id="beneficios">
+        <div class="interface">
+            <div class="txt-container">
+                <h2>Beneficios</h2>
+                <p>
+                    Gerencie todos os aspectos da sua vida financeira com nossas ferramentas intuitivas.
+                </p>
+            </div>
+            <div class="cards-container">
+                <div class="card">
+                    <i class="bi bi-wallet"></i>
+                    <h3>Rendas</h3>
                     <p>
-                        Simplifique o controle de rendas, despesas e investimentos em um só lugar. Tome decisões
-                        financeiras com confiança e alcance seus objetivos.
+                        Registre e categorize suas fontes de renda. Visualize a evolução e planeje com base em
+                        expectativas futuras.
                     </p>
-                    <a id="btn-cadastro" href="<?php echo BASE_URL ?>cadastro.php">Abrir conta</a>
                 </div>
-                <div class="right">
-                    <img src="<?php echo BASE_URL ?>assets/img/index_img.png" alt="Hero">
-                </div>
-            </div>
-        </section>
-        <section id="beneficios">
-            <div class="interface">
-                <div class="txt-container">
-                    <h2>Beneficios</h2>
+                <div class="card">
+                    <i class="bi bi-graph-down-arrow"></i>
+                    <h3>Despesas</h3>
                     <p>
-                        Gerencie todos os aspectos da sua vida financeira com nossas ferramentas intuitivas.
+                        Controle suas despesas pagas e pendentes. Identifique padrões e reduza gastos
+                        desnecessários.
                     </p>
                 </div>
-                <div class="cards-container">
-                    <div class="card">
-                        <i class="bi bi-wallet"></i>
-                        <h3>Rendas</h3>
-                        <p>
-                            Registre e categorize suas fontes de renda. Visualize a evolução e planeje com base em
-                            expectativas futuras.
-                        </p>
+                <div class="card">
+                    <i class="bi bi-graph-up-arrow"></i>
+                    <h3>Investimentos</h3>
+                    <p>Acompanhe o desempenho dos seus investimentos. Análise histórica e projeções para o futuro.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="vantagens">
+        <div class="interface">
+            <div class="txt-van">
+                <p>Por que escolher EcoFlow?</p>
+                <h3>Tome controle da sua vida financeira</h3>
+                <div class="container-vantagens">
+                    <div class="vantagem">
+                        <i class="bi bi-check2-circle"></i>
+                        <div class="txt-vantagem">
+                            <p class="titulo">
+                                Visão clara das suas finanças
+                            </p>
+                            <p>Veja exatamente para onde vai seu dinheiro e como otimizar seus recursos.</p>
+                        </div>
                     </div>
-                    <div class="card">
-                        <i class="bi bi-graph-down-arrow"></i>
-                        <h3>Despesas</h3>
-                        <p>
-                            Controle suas despesas pagas e pendentes. Identifique padrões e reduza gastos
-                            desnecessários.
-                        </p>
+                    <div class="vantagem">
+                        <i class="bi bi-check2-circle"></i>
+                        <div class="txt-vantagem">
+                            <p class="titulo">
+                                Nunca mais perca prazos
+                            </p>
+                            <p>Receba alertas sobre contas a pagar e evite juros e multas desnecessárias.</p>
+                        </div>
                     </div>
-                    <div class="card">
-                        <i class="bi bi-graph-up-arrow"></i>
-                        <h3>Investimentos</h3>
-                        <p>Acompanhe o desempenho dos seus investimentos. Análise histórica e projeções para o futuro.
-                        </p>
+                    <div class="vantagem">
+                        <i class="bi bi-check2-circle"></i>
+                        <div class="txt-vantagem">
+                            <p class="titulo">
+                                Gráficos e relatórios detalhados
+                            </p>
+                            <p>Analise seu comportamento financeiro com visualizações claras e objetivas.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
-        <section id="vantagens">
-            <div class="interface">
-                <div class="txt-van">
-                    <p>Por que escolher EcoFlow?</p>
-                    <h3>Tome controle da sua vida financeira</h3>
-                    <div class="container-vantagens">
-                        <div class="vantagem">
-                            <i class="bi bi-check2-circle"></i>
-                            <div class="txt-vantagem">
-                                <p class="titulo">
-                                    Visão clara das suas finanças
-                                </p>
-                                <p>Veja exatamente para onde vai seu dinheiro e como otimizar seus recursos.</p>
-                            </div>
-                        </div>
-                        <div class="vantagem">
-                            <i class="bi bi-check2-circle"></i>
-                            <div class="txt-vantagem">
-                                <p class="titulo">
-                                    Nunca mais perca prazos
-                                </p>
-                                <p>Receba alertas sobre contas a pagar e evite juros e multas desnecessárias.</p>
-                            </div>
-                        </div>
-                        <div class="vantagem">
-                            <i class="bi bi-check2-circle"></i>
-                            <div class="txt-vantagem">
-                                <p class="titulo">
-                                    Gráficos e relatórios detalhados
-                                </p>
-                                <p>Analise seu comportamento financeiro com visualizações claras e objetivas.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="grafico-van">
-                    <div class="txt-grafico">
-                        <i class="bi bi-pie-chart"></i>
+            <div class="grafico-van">
+                <div class="txt-grafico">
+                    <i class="bi bi-bar-chart"></i>
+                    <div class="container-txt">
                         <p class="titulo">Distribuição de despesas</p>
                         <p>Visão geral de maio 2024</p>
                     </div>
                 </div>
-            </div>
-        </section>
-        <footer>
-            <div class="interface">
-                <div class="footer-content">
-                    <div class="footer-left">
-                        <h3>Eco Flow</h3>
-                        <p>Seu parceiro na jornada rumo à liberdade financeira.</p>
-                    </div>
-
-                    <div class="footer-links">
-                        <h4>Links rápidos</h4>
-                        <ul>
-                            <li><a href="login.php">Login</a></li>
-                            <li><a href="cadastro.php">Abrir conta</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="footer-contact">
-                        <h4>Contato</h4>
-                        <p>Email: suporte@ecoflow.com</p>
-                        <p>Instagram: @eco.flow</p>
-                    </div>
-                </div>
-                <div class="footer-bottom">
-                    <p>&copy; <?php echo date('Y'); ?> Eco Flow. Todos os direitos reservados.</p>
+                <img src="assets/img/grafico.png" alt="Gráfico">
+                <div class="info-grafico">
+                    <div class="info"><i class="bi bi-circle-fill" style="color: #0077b6;"></i>Rendas</div>
+                    <div class="info"><i class="bi bi-circle-fill" style="color: #d00000;"></i>Despesas</div>
+                    <div class="info"><i class="bi bi-circle-fill" style="color: #ffba08;"></i>Investimentos</div>
                 </div>
             </div>
-        </footer>
-    </div>
+        </div>
+    </section>
+    <footer>
+        <div class="interface">
+            <div class="footer-content">
+                <div class="footer-left">
+                    <h3>Eco Flow</h3>
+                    <p>Seu parceiro na jornada rumo à liberdade financeira.</p>
+                </div>
+
+                <div class="footer-links">
+                    <h4>Links rápidos</h4>
+                    <ul>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="cadastro.php">Abrir conta</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-contact">
+                    <h4>Contato</h4>
+                    <p>Email: suporte@ecoflow.com</p>
+                    <p>Instagram: @eco.flow</p>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p>&copy; <?php echo date('Y'); ?> Eco Flow. Todos os direitos reservados.</p>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
